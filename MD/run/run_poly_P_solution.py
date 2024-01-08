c12ee0cbaeeb63dedfe848e0942e422413f42f4a
Khondamir Rustamov <69417652+KhondamirRustamov@users.noreply.github.com>
2024-01-08 15:11:35 +0500

    Update run_poly_P_solution.py

--- MD/run/run_poly_P_solution.py
+++ MD/run/run_poly_P_solution.py
@@ -6,13 +6,13 @@ equi_prefix = 'step4.1_equilibration'
 prod_prefix = 'step5_production'
 prod_step   = 'step5'
 
-os.system(f'gmx grompp -f {mini_prefix}.mdp -o {mini_prefix}.tpr -c {init}.gro -r {init}.gro -p topol.top -n index.ndx -maxwarn -1')
+os.system(f'gmx grompp -f {mini_prefix}.mdp -o {mini_prefix}.tpr -c {init}.gro -r {init}.gro -p topol.top -n index.ndx -maxwarn')
 os.system(f'gmx mdrun -v -deffnm {mini_prefix} ')
 
-os.system(f'gmx grompp -f {equi_prefix}.mdp -o {equi_prefix}.tpr -c {mini_prefix}.gro -r {init}.gro -p topol.top -n index.ndx -maxwarn -1')
+os.system(f'gmx grompp -f {equi_prefix}.mdp -o {equi_prefix}.tpr -c {mini_prefix}.gro -r {init}.gro -p topol.top -n index.ndx -maxwarn')
 os.system(f'gmx mdrun -v -deffnm {equi_prefix}')
 
-os.system(f'gmx grompp -f {prod_prefix}.mdp -o final.tpr -c {equi_prefix}.gro -p topol.top -n index.ndx -maxwarn -1')
+os.system(f'gmx grompp -f {prod_prefix}.mdp -o final.tpr -c {equi_prefix}.gro -p topol.top -n index.ndx -maxwarn')
 os.system(f'gmx mdrun -v -deffnm final')
 
 
